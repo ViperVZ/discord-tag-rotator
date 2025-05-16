98c332138e6d7a03e4643316b106bb82f861d771
V <96781114+ViperVZ@users.noreply.github.com>
2025-05-16 18:43:01 +0800

    Create rotator.py

---
+++ rotator.py
@@ -0,0 +1,68 @@
+import requests # Make sure u install this module
+import time
+
+Token = "" # Put ur account token here
+Delay = 10 # Rotation Delay
+
+Server_IDS = [
+    "", # Put server id that has a guild tag - Unli server btw
+    "",
+]
+
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 14_7_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/92.0.4515.159 Mobile/15E148 Safari/604.1",
+    "Accept": "*/*",
+    "Content-Type": "application/json",
+    "Authorization": Token,
+    "Origin": "https://discord.com",
+    "Referer": "https://discord.com/channels/@me",
+}
+
+def rotator():
+    print("[*] Starting Guild Tag Rotator by discord.gg/rippers | @coolishsec \n")
+    while True:
+        for server_id in Server_IDS:
+            payload = {
+                "identity_guild_id": server_id,
+                "identity_enabled": True
+            }
+            try:
+                response = requests.put(
+                    "https://discord.com/api/v9/users/@me/clan",
+                    headers=HEADERS,
+                    json=payload
+                )
+                if response.ok:
+                    data = response.json()
+                    clan = data.get("clan", {})
+                    tag = clan.get("tag", "unknown")
+                    print(f"[+] Updated to guild tag: {tag} | Server ID: {server_id}")
+                else:
+                    print(f"[!] Failed to update tag for {server_id} (Status {response.status_code})")
+                    print(f"    Response: {response.text}")
+
+            except requests.exceptions.RequestException as e:
+                print(f"[!] Request error for guild {server_id}: {e}")
+
+            time.sleep(Delay)
+
+if __name__ == "__main__":
+    print("""⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢤⣶⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⡾⠿⢿⡀⠀⠀⠀⠀⣠⣶⣿⣷⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣦⣴⣿⡋⠀⠀⠈⢳⡄⠀⢠⣾⣿⠁⠈⣿⡆⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⠿⠛⠉⠉⠁⠀⠀⠀⠹⡄⣿⣿⣿⠀⠀⢹⡇⠀⠀⠀
+⠀⠀⠀⠀⠀⣠⣾⡿⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⣰⣏⢻⣿⣿⡆⠀⠸⣿⠀⠀⠀
+⠀⠀⠀⢀⣴⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⣿⣿⣆⠹⣿⣷⠀⢘⣿⠀⠀⠀
+⠀⠀⢀⡾⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⣿⠋⠉⠛⠂⠹⠿⣲⣿⣿⣧⠀⠀
+⠀⢠⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣿⣿⣿⣷⣾⣿⡇⢀⠀⣼⣿⣿⣿⣧⠀
+⠰⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⡘⢿⣿⣿⣿⠀
+⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⣷⡈⠿⢿⣿⡆
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠛⠁⢙⠛⣿⣿⣿⣿⡟⠀⡿⠀⠀⢀⣿⡇
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣶⣤⣉⣛⠻⠇⢠⣿⣾⣿⡄⢻⡇
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣦⣤⣾⣿⣿⣿⣿⣆⠁
+""")
+    
+    if not Token:
+        print("[-] ERROR: Set your Token before running the script.")
+    else:
+        rotator()
